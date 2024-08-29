f5e472a9d18fbf525398d9bac20924ba3d857d9e
matthewdelorenzo <matthewdelorenzo@tamu.edu>
2024-08-29 12:50:48 -0500

    fixing bash for Olympus cluster

--- mcts_verilogeval.sh
+++ mcts_verilogeval.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=mcts_vereval        # Job name
+#SBATCH --mail-type=ALL                  # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=matthewdelorenzo@tamu.edu  # Where to send mail
+#SBATCH --nodes=1                        # Use one node
+#SBATCH --ntasks=1                       # Run a single task
+#SBATCH --cpus-per-task=1                # Number of CPU cores per task
+#SBATCH --gres=gpu:a100:1               # Type and number of GPUs 
+#SBATCH --partition=gpu-research                  # Partition/Queue to run in
+#SBATCH --qos=olympus-research-gpu       # Set QOS to use
+#SBATCH --time=96:00:00                  # Time limit hrs:min:sec - set to 1 hour
+
 DUMPDIR="/mnt/shared-scratch/Rajendran_J/matthewdelorenzo/github_mcts/MCTS/dump/"
 MODULE_NAME="top_module"
 MODEL_NAME="shailja/fine-tuned-codegen-2B-Verilog"
