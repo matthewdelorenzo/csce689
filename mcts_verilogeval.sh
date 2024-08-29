47eef696082111fd0c01981ab9513d9c173fcfc2
matthewdelorenzo <matthewdelorenzo@tamu.edu>
2024-08-29 12:32:36 -0500

    fixing bash3

--- mcts_verilogeval.sh
+++ mcts_verilogeval.sh
@@ -1,8 +1,8 @@
 DUMPDIR="/mnt/shared-scratch/Rajendran_J/matthewdelorenzo/github_mcts/MCTS/dump/"
 MODULE_NAME="top_module"
-MODEL_NAME="shailja/fine-tuned-codegen-16B-Verilog"
+MODEL_NAME="shailja/fine-tuned-codegen-2B-Verilog"
 CSV_FILES="/mnt/shared-scratch/Rajendran_J/matthewdelorenzo/github_mcts/MCTS/ver16b_vereval_mcts_test1.csv"
-python main_og.py \
+python -u main_og.py \
         --dumpdir "$DUMPDIR" \
         --module_name "$MODULE_NAME" \
         --model_name "$MODEL_NAME" \
